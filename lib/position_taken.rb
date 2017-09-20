f6eaaafd22dc65423c2a130058c165853b74c884
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-20 00:51:52 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,5 @@
 # code your #position_taken? method here!
 def position_taken?(board, position)
-  position = position.to_i - 1
   if (board[position] == " " || board[position] == "" || board[position] == nil)
     puts board[position], "false"
     false
