afb1fa3c907350c5b64d35fe26b04fbb57f997a0
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-20 00:46:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,7 @@
 def position_taken?(board, position)
   position = position.to_i - 1
   if (board[position] == " " || board[position] == "" || board[position] == nil)
+    puts "false"
     false
   else
     true
